fe1d56226d64a85af8950337c896bbac8cba09d5
Татьяна Антонова <t4n4uk@gmail.com>
2023-05-24 23:50:25 +0300

    ВСЕ ЗАКОНЧИЛОСЬ

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -9,6 +8,8 @@ public class Main {
 
             Scanner scan = new Scanner(System.in);
             boolean exit = false;
+
+            MyDictionary mD = new MyDictionary("^[a-zA-Z]{4}$","^\\d{5}$");
 
             String path = "";
 
@@ -29,18 +30,25 @@ public class Main {
                         path = "";
                         System.out.println("Введите путь к файлу: ");
                         path = scan.next();
+                        mD.readFromFile(path);
+                        mD.printDictionary();
                         break;
                     case 2:
+                        mD.printDictionary();
                         break;
                     case 3:
 
                         System.out.println("Введите ключ по которому необходимо удалить запись в словаре: ");
                         String kdelete = scan.next();
+                        mD.deleteKey(kdelete);
+
                         break;
                     case 4:
 
                         System.out.println("Введите ключ по которому необходимо найти запись в словаре: ");
                         String ksearch = scan.next();
+                        String val =  mD.searchKey(ksearch);
+                        System.out.println("Значение по запрошеному ключу: " + val);
                         break;
                     case 5:
 
@@ -49,8 +57,11 @@ public class Main {
                         System.out.println("Введите значение: ");
                         String valueAdd = scan.next();
                         keyAdd += " - " + valueAdd;
+                        mD.dictionaryPutString(keyAdd);
+
                         break;
                     case 6:
+                        mD.swapValueToKey();
                         System.out.println("Словарь перевернут");
                         break;
                     case 7:
